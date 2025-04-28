5670c628d3631f276b601c93dad66cf64973d2c4
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-04-28 12:01:15 -0300

    commit atualizando

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Red Hat Node.js! Agora com o deploy atualizado!');
+  res.send('Hello from Red Hat Node.js! Agora com o deploy atualizado, mas com nome do projeto diferente');
 });
 
 app.listen(port, () => {
